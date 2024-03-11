4ca5016dff386d565502f043afe51c30212f5740
Tempest <l1641101764@gmail.com>
2024-03-11 13:50:23 +0800

    first commit

--- python_scripts/debug_tools.py
+++ python_scripts/debug_tools.py
@@ -3,6 +3,7 @@ from typing import Any, Callable
 
 
 def traceback_call_stack(func: Callable) -> Callable:
+    
     def wrapper(*args, **kwargs) -> Any:
         for frame in traceback.format_stack():
             print(frame.strip())
